8be160e4d446788436c61e4da8f8ad1ef8ecd1e0
bencebecsy <becsy.bence@gmail.com>
2022-05-06 15:10:17 -0600

    Add shell script to run analysis

---
+++ run_QuickCW.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#It can be beneficial to set the numba thread number to the number of physical cores of the machine used
+export NUMBA_NUM_THREADS=12
+export MKL_NUM_THREADS=12
+
+#nice -n 20 /usr/bin/time python -u runQuickMCMC.py
+python -u runQuickMCMC.py
